ef2f7d2c8512f9863d0d945176c4f16f04f2c7ea
Salmandarzi <92723853+Salmandarzi@users.noreply.github.com>
2022-01-23 23:05:34 +0530

    Update rock-paper-scissors-game.py

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,8 +1,8 @@
-#import library
+
 from tkinter import *
 import random
 
-#initialize window
+
 root = Tk()
 root.geometry('400x400')
 root.resizable(0,0)
@@ -10,18 +10,18 @@ root.title('DataFlair-Rock,Paper,Scissors')
 root.config(bg ='seashell3')
 
 
-#heading
+
 Label(root, text = 'Rock, Paper ,Scissors' , font='arial 20 bold', bg = 'seashell2').pack()
 
 
-##user choice
+
 user_take = StringVar()
 Label(root, text = 'choose any one: rock, paper ,scissors' , font='arial 15 bold', bg = 'seashell2').place(x = 20,y=70)
 Entry(root, font = 'arial 15', textvariable = user_take , bg = 'antiquewhite2').place(x=90 , y = 130)
 
 
 
-#computer choice
+
 comp_pick = random.randint(1,3)
 if comp_pick == 1:
     comp_pick = 'rock'
@@ -33,7 +33,7 @@ else:
 
 
 
-##function to play
+
 Result = StringVar()
 
 def play():
@@ -57,17 +57,17 @@ def play():
     
         
     
-##fun to reset
+
 def Reset():
     Result.set("") 
     user_take.set("")
 
-##fun to exit
+
 def Exit():
     root.destroy()
 
 
-###### button
+
 Entry(root, font = 'arial 10 bold', textvariable = Result, bg ='antiquewhite2',width = 50,).place(x=25, y = 250)
 
 Button(root, font = 'arial 13 bold', text = 'PLAY'  ,padx =5,bg ='seashell4' ,command = play).place(x=150,y=190)
